06c55f6ddabfc788b9815105e304680a66e98e60
Joe Maller <joe@joemaller.com>
2023-08-17 18:08:12 -0400

    restore bump-phpunit script

--- bin/bump-phpunit.sh
+++ bin/bump-phpunit.sh
@@ -3,7 +3,7 @@
 # This script is a part of the ideasonpurpose/docker-phpunit-watch project
 # https://github.com/ideasonpurpose/docker-phpunit-watch
 #
-# Version: 1.2.0
+# Version: 0.9.4
 #
 # For now, manually update the PHPUnit version in phpunit-version.json before running
 #
@@ -15,16 +15,19 @@
 # GitHub Action uses this file to generate tag names for the Docker Image.
 
 # style helpers
-RESET="\033[0m" BOLD="\033[1m" RED="\033[31m" GREEN="\033[32m" GOLD="\033[33m"
-BLUE="\033[34m" MAGENTA="\033[35m" CYAN="\033[36m"
+RESET="\033[0m"
+BOLD="\033[1m"
+RED="\033[31m"
+GREEN="\033[32m"
+GOLD="\033[33m"
+BLUE="\033[34m"
+MAGENTA="\033[35m"
+CYAN="\033[36m"
 
-PHPUNIT_LATEST=$(jq -r 'to_entries[].value' /app/phpunit-version.json)
+PHPUNIT_LATEST=$(jq -r '.phpunit' /app/phpunit-version.json)
 
-echo -e "✏️   Updating ${GOLD}Dockerfile${RESET} to
-${CYAN}phpunit-${PHPUNIT_LATEST}.phar${RESET}" sed -i
-"s/phpunit.de\/phpunit-.*phar/phpunit.de\/phpunit-${PHPUNIT_LATEST}.phar/"
-/app/Dockerfile
+echo -e "✏️   Updating ${GOLD}Dockerfile${RESET} to ${CYAN}phpunit-${PHPUNIT_LATEST}.phar${RESET}"
+sed -i "s/phpunit.de\/phpunit-.*phar/phpunit.de\/phpunit-${PHPUNIT_LATEST}.phar/" /app/Dockerfile
 
-echo -e "✏️   Updating ${GOLD}README.md${RESET} to
-${CYAN}v${PHPUNIT_LATEST}${RESET}" sed -E -i "s/^<\!-- PHPUNIT_VERSION -->-
-PHPUnit.+$/<\!-- PHPUNIT_VERSION -->- PHPUnit ${PHPUNIT_LATEST}/" /app/README.md
+echo -e "✏️   Updating ${GOLD}README.md${RESET} to ${CYAN}v${PHPUNIT_LATEST}${RESET}"
+sed -E -i "s/^<\!-- PHPUNIT_VERSION -->- PHPUnit.+$/<\!-- PHPUNIT_VERSION -->- PHPUnit ${PHPUNIT_LATEST}/" /app/README.md
