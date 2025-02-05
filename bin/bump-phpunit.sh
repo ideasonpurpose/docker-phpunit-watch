ac7a7c0fcfd81b8addb7728e6f30180e8453f0da
Joe Maller <joe@joemaller.com>
2025-02-04 22:51:52 -0500

    update bump to handle php and phpunit versions

--- bin/bump-phpunit.sh
+++ bin/bump-phpunit.sh
@@ -24,10 +24,13 @@ BLUE="\033[34m"
 MAGENTA="\033[35m"
 CYAN="\033[36m"
 
-PHPUNIT_LATEST=$(jq -r '.phpunit' /app/phpunit-version.json)
+PHP_LATEST=$(jq -r '.php' /app/php-versions.json)
+PHPUNIT_LATEST=$(jq -r '.phpunit' /app/php-versions.json)
 
-echo -e "✏️   Updating ${GOLD}Dockerfile${RESET} to ${CYAN}phpunit-${PHPUNIT_LATEST}.phar${RESET}"
+echo -e "✏️   Updating ${GOLD}Dockerfile${RESET} to ${CYAN}php-${PHP_LATEST}${RESET} and ${CYAN}phpunit-${PHPUNIT_LATEST}.phar${RESET}"
+sed -i "s/FROM php:.*-cli/FROM php:${PHP_LATEST}-cli/" /app/Dockerfile
 sed -i "s/phpunit.de\/phpunit-.*phar/phpunit.de\/phpunit-${PHPUNIT_LATEST}.phar/" /app/Dockerfile
 
-echo -e "✏️   Updating ${GOLD}README.md${RESET} to ${CYAN}v${PHPUNIT_LATEST}${RESET}"
+echo -e "✏️   Updating ${GOLD}README.md${RESET} to ${CYAN}PHP ${PHP_LATEST}${RESET} and ${CYAN}PHPUnit v${PHPUNIT_LATEST}${RESET}"
 sed -E -i "s/^<\!-- PHPUNIT_VERSION -->- PHPUnit.+$/<\!-- PHPUNIT_VERSION -->- PHPUnit ${PHPUNIT_LATEST}/" /app/README.md
+sed -E -i "s/^<\!-- PHP_VERSION -->- PHP.+$/<\!-- PHP_VERSION -->- PHP ${PHP_LATEST}/" /app/README.md
